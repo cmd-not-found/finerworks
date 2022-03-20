8ade455a247958bb99de53165f5ae44359cb58e8
cmd-not-found <cmd404notfound@gmail.com>
2022-03-19 21:53:20 -0400

    v0.0.2: add more endpoints and clean up

--- finerworks/__init__.py
+++ finerworks/__init__.py
@@ -14,6 +14,7 @@ class api:  # pylint: disable=invalid-name
         """
         Initialize FinerWorks API with required values.
         """
+
         if web_api_key == None and app_key == None:
             raise FinerworksError(
                 msg='\n`web_api_key` and `app_key` are required.'
@@ -28,6 +29,7 @@ class api:  # pylint: disable=invalid-name
         """
         Execute HTTP API requests for Finerworks REST API.
         """
+
         headers = {
             "web_api_key": self.web_api_key,
             "app_key": self.app_key,
@@ -46,8 +48,8 @@ class api:  # pylint: disable=invalid-name
         elif resp.status_code == 400:
             return resp.json()
         else:
-            raise ConnectionError(
-                "Could Not Connect. Status Code: {0}".format(resp.status_code)
+            raise FinerworksError(
+                msg="Could Not Connect. Status Code: {0}".format(resp.status_code)
             )
 
     @staticmethod
@@ -55,6 +57,7 @@ class api:  # pylint: disable=invalid-name
         """
         Validate Order ID type.
         """
+
         if isinstance(current_id, str):
             try:
                 order_id = int(current_id)
@@ -72,10 +75,15 @@ class api:  # pylint: disable=invalid-name
         """
         Attempt to authenticate to API and test connection.
         """
+
         if not self.web_api_key:
-            raise TypeError("Missing web_api_key")
+            raise FinerworksError(
+                msg="Missing `web_api_key`"
+            )
         if not self.app_key:
-            raise TypeError("Missing app_key")
+            raise TypeError(
+                msg="Missing `app_key`"
+            )
 
         req = self._http_req("/test_my_credentials")
         return req
@@ -94,6 +102,7 @@ class api:  # pylint: disable=invalid-name
         Submit a FinerWorks Order via REST API.
         """
 
+        # TODO keyword args
         order = {
             "order_po": order_no,
             "recipient": recipient,
@@ -112,22 +121,24 @@ class api:  # pylint: disable=invalid-name
         """
         Update a FinerWorks Order via REST API.
         """
+
         order_id = self._validate_order_id(order_id)
         options = ["pending", "hold", "cancel"]
         if status.lower() not in options:
-            raise ValueError(
-                "Order status update not in valid state options: {}".format(
+            raise FinerworksError(
+                msg="Order status update not in valid state. Options: {}".format(
                     ",".join(options)
                 )
             )
         order_update = {"order_id": order_id, "update_command": status.lower()}
-        req = self._http_req("/", order_update)
+        req = self._http_req("/update_order", order_update)
         return req
 
     def order_status(self, curr_id):
         """
         Retrieve FinerWorks Order Status via REST API.
         """
+
         order_id = self._validate_order_id(curr_id)
         orders = {"order_ids": [order_id]}
         req = self._http_req("/fetch_order_status", orders)
@@ -145,7 +156,7 @@ class api:  # pylint: disable=invalid-name
         """
         Retrieve Order Shipping options via REST API.
         """
-        pass  # pylint: disable=unnecessary-pass
+        pass
 
     def customer_update(self):
         """
@@ -173,6 +184,71 @@ class api:  # pylint: disable=invalid-name
         """
         Query Product Images via REST API.
         """
+
         q_filter = {"search_filter": query}
         req = self._http_req("/list_images", body=q_filter)
         return req
+
+    def frame_collections(self, collection_id=None):
+        """
+        Query Frame Collections via REST API.
+        """
+
+        if not collection_id:
+            raise FinerworksError(
+                msg="`collection_id` required to query Frame Collection details."
+            )
+        else:
+            c_id = {
+                "id": collection_id
+            }
+            req = self._http_req("/list_collections", body=c_id)
+            return req
+
+    def frame_details(self, frame_id=None):
+        """
+        Query for Specific Frame Details via REST API.
+        """
+
+        if not frame_id:
+            raise FinerworksError(
+                msg="`frame_id` required to query frame details."
+            )
+        else:
+            f_id = {
+                "id": frame_id
+            }
+            req = self._http_req("/frame_details", body=f_id)
+            return req
+
+    def frame_mats(self, mat_id=None):
+        """
+        Query for Frame Mat Options via REST API.
+        """
+
+        if not mat_id:
+            raise FinerworksError(
+                msg="`mat_id` required to query Frame Mat details."
+            )
+        else:
+            m_id = {
+                "id": mat_id
+            }
+            req = self._http_req("/list_mats", body=m_id)
+            return req
+
+    def frame_glazing(self, glazing_id=None):
+        """
+        Query for Frame Glazing Options via REST API.
+        """
+
+        if not glazing_id:
+            raise FinerworksError(
+                msg="`glazing_id` required to query Frame Glazing details."
+            )
+        else:
+            g_id = {
+                "id": glazing_id
+            }
+            req = self._http_req("/list_glazing", body=g_id)
+            return req
